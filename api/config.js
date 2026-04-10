863cd9e4708986d8e8ed8925e92d5559c8f5c3e2
Paul Ukah <81622138+ukahip@users.noreply.github.com>
2026-04-10 12:07:10 +0100

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    gatewayUrl:      process.env.GATEWAY_URL           || '',
-    identityApiKey:  process.env.IDENTITY_API_KEY      || '',
+    gatewayUrl:     process.env.GATEWAY_URL      || '',
+    identityApiKey: process.env.IDENTITY_API_KEY || '',
   });
 }
